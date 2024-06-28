afd078b4f64e748b38e5870e47b86cd14a68ac54
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-06-28 12:30:56 -0400

    Completed isEmpty string method

--- src/IsEmptyExample.java
+++ src/IsEmptyExample.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class IsEmptyExample {
+public class IsEmptyExample {
+
+    public static void main(String args[]){
+        String s1 = "";
+        String s2 = "hello";
+
+        System.out.println(s1.isEmpty());
+        System.out.println(s2.isEmpty());
+    }
 }
