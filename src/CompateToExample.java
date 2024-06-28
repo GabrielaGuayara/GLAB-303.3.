ba0d8e02995a9b546a0f01b8c9856fb50812ee0e
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-06-28 12:32:42 -0400

    Completed compareto string method

--- src/CompateToExample.java
+++ src/CompateToExample.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class CompateToExample {
+public class CompateToExample {
+    public static void main(String args[]){
+        String s1 = "hello";
+        String s2 = "hello";
+        String s3 = "hemlo";
+        String s4 = "flag";
+
+        System.out.println(s1.compareTo(s2));
+
+        System.out.println(s1.compareTo(s3));
+
+        System.out.println(s1.compareTo(s4));
+    }
 }
