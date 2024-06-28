50c7b31e3c71de63e2d17cfc2fba6870576e9904
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-06-28 12:32:01 -0400

    Completed concatDemo

--- src/concatDemo.java
+++ src/concatDemo.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class concatDemo {
+public class concatDemo {
+
+    public static void main(String [] args){
+        String str1 = "Learn ";
+        String str2 = "Java";
+
+        System.out.println(str1.concat(str2));
+
+        System.out.println(str2.concat(str1));
+
+        String s3 = "hello";
+        String s4 = "Learners";
+
+        String s5 = s3 + s4;
+        System.out.println(s5);
+
+        String message = "Welcome " + "to "+ "Java";
+        System.out.println(message);
+
+        String s = "Chapter " + 2;
+
+        String s1 = "Supplement " + 'B';
+        System.out.println(s1);
+    }
 }
